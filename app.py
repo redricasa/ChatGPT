c436fd506beac54dc548d950e8a4d200fcbab48a
redricasa <fredeb26@gmail.com>
2023-04-17 17:48:13 -0700

    attempting to debug API response

--- app.py
+++ app.py
@@ -10,3 +10,15 @@ load_dotenv()
 openai.api_key = os.getenv('KEY')
 
 # print("api key " + openai.api_key)
+def BasicGeneration(userPrompt):
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role" : "user", "content" : userPrompt}
+        ]
+    )
+    return completion.choices[0].message.content
+
+prompt = "what is a comet?"
+response = BasicGeneration(prompt)
+print(response)
