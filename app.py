6d891ca998dca0abbaf120f348edeb5eb5c09e9f
redricasa <fredeb26@gmail.com>
2023-05-20 22:10:38 -0700

    used ChatGPT to debug and changed the classes PLUS, I've reached my API prompt limit!

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import openai
+
 import os
 from dotenv import load_dotenv
+import openai
 # from pathlib import Path
 
 
@@ -11,14 +12,33 @@ openai.api_key = os.getenv('KEY')
 
 # print("api key " + openai.api_key)
 def BasicGeneration(userPrompt):
-    completion = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = [
-            {"role" : "user", "content" : userPrompt}
-        ]
+    completion = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=userPrompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.7,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        log_level="info",
+        # model = "gpt-3.5-turbo",
+        # messages = [
+        #     {"role" : "user", "content" : userPrompt}
+        # ]
     )
-    return completion.choices[0].message.content
+    return completion.choices[0].text.strip()
 
-prompt = "what is a comet?"
+prompt = "what is a comet"
 response = BasicGeneration(prompt)
 print(response)
+
+# response = openai.Completion.create(
+#     model="text-davinci-003", 
+#     prompt="Say this is a test", 
+#     temperature=0.6, 
+#     max_tokens=150
+#     )
+
+# print(response.choices[0].text)
